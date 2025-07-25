b60dafe91c896be824b73cc02b8850bf4573bab8
Demba Siby <dembasiby@gmail.com>
2025-07-25 05:57:15 +0000

    add get book text function

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+
+def main():
+    text = get_book_text("./books/frankenstein.txt")
+    print(text)
+
+main()
+
